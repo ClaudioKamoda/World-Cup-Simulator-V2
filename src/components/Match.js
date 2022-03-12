6dbba08f63610ab90ec442b7543fd71bff165375
Claudio <claudiojlf@gmail.com>
2022-03-11 21:12:03 -0300

    score can be edited now

--- src/components/Match.js
+++ src/components/Match.js
@@ -6,8 +6,8 @@ import FeatherIcon from 'feather-icons-react'
 export const Match = props => {
 	const [isEditing, setIsEditing] = useState(false)
 	const [buttonIcon, setButtonIcon] = useState('edit-3')
-	let scoreA = 0
-	let scoreB = 0
+	const [scoreA, setScoreA] = useState(0)
+	const [scoreB, setScoreB] = useState(0)
 
 	const toggleEdit = () => {
 		setIsEditing(prevState => {
@@ -17,6 +17,18 @@ export const Match = props => {
 			if (prevState === 'edit-3') return 'check'
 			else return 'edit-3'
 		})
+	}
+
+	const handleScoreA = e => {
+		setScoreA(e.target.value)
+	}
+
+	const handleScoreB = e => {
+		setScoreB(e.target.value)
+	}
+
+	const handleSubmit = e => {
+		e.preventDefault()
 	}
 
 	return (
@@ -35,15 +47,25 @@ export const Match = props => {
 			<p className="match__teamA">{props.matchData.team_A}</p>
 			{!isEditing && <p className="match__scoreA">{scoreA}</p>}
 			{isEditing && (
-				<form className="match__scoreA">
-					<input type="text" value={scoreA} maxlength="2" />
+				<form className="match__scoreA" onSubmit={handleSubmit}>
+					<input
+						type="text"
+						value={scoreA}
+						onChange={handleScoreA}
+						maxlength="2"
+					/>
 				</form>
 			)}
 			<p className="match__cross">x</p>
 			{!isEditing && <p className="match__scoreB">{scoreB}</p>}
 			{isEditing && (
-				<form className="match__scoreB">
-					<input type="text" value={scoreB} maxlength="2" />
+				<form className="match__scoreB" onSubmit={handleSubmit}>
+					<input
+						type="text"
+						value={scoreB}
+						onChange={handleScoreB}
+						maxlength="2"
+					/>
 				</form>
 			)}
 			<p className="match__teamB">{props.matchData.team_B}</p>
