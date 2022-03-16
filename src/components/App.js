ac88e8b024600c9d3be0f69b2a0a5bc52ef7946f
Claudio <claudiojlf@gmail.com>
2022-03-16 16:53:34 -0300

    added group component but not working properly yet

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 import { MatchList } from './MatchList'
 import { GroupNav } from './GroupNav'
+import { Group } from './Group'
 import '../css/styles/App.scss'
 
 export const App = () => {
 	return (
 		<div className="App">
 			<GroupNav />
+			<Group />
 			<MatchList />
 		</div>
 	)
--- src/store/store.js
+++ src/store/store.js
@@ -1,7 +1,227 @@
 import { createStore } from 'redux'
 
 const INITIAL_STATE = {
-	currentGroup: 'A'
+	currentGroup: 'A',
+	groups: {
+		A: {
+			name: 'Group A',
+			teams: [
+				{
+					team_name: 'Brazil',
+					team_short: 'BRA',
+					points: 0,
+					matches_played: 0,
+					wins: 0,
+					draws: 0,
+					losses: 0,
+					goals_forward: 0,
+					goals_against: 0,
+					goal_difference: 0
+				},
+				{
+					team_name: 'Argentina',
+					team_short: 'ARG',
+					points: 0,
+					matches_played: 0,
+					wins: 0,
+					draws: 0,
+					losses: 0,
+					goals_forward: 0,
+					goals_against: 0,
+					goal_difference: 0
+				},
+				{
+					team_name: 'France',
+					team_short: 'FRA',
+					points: 0,
+					matches_played: 0,
+					wins: 0,
+					draws: 0,
+					losses: 0,
+					goals_forward: 0,
+					goals_against: 0,
+					goal_difference: 0
+				},
+				{
+					team_name: 'Germany',
+					team_short: 'GER',
+					points: 0,
+					matches_played: 0,
+					wins: 0,
+					draws: 0,
+					losses: 0,
+					goals_forward: 0,
+					goals_against: 0,
+					goal_difference: 0
+				}
+			],
+			matches: [
+				{
+					id: 1,
+					team_A: 'BRA',
+					score_A: 0,
+					team_B: 'GER',
+					score_B: 0,
+					match_time: '13:00',
+					stadium: 'Education City'
+				},
+				{
+					id: 2,
+					team_A: 'FRA',
+					score_A: 0,
+					team_B: 'ARG',
+					score_B: 0,
+					match_time: '16:00',
+					stadium: 'Ahmed bin Ali'
+				},
+				{
+					id: 3,
+					team_A: 'FRA',
+					score_A: 0,
+					team_B: 'BRA',
+					score_B: 0,
+					match_time: '16:00',
+					stadium: 'Ahmed bin Ali'
+				},
+				{
+					id: 4,
+					team_A: 'GER',
+					score_A: 0,
+					team_B: 'ARG',
+					score_B: 0,
+					match_time: '16:00',
+					stadium: 'Ahmed bin Ali'
+				},
+				{
+					id: 5,
+					team_A: 'BRA',
+					score_A: 0,
+					team_B: 'ARG',
+					score_B: 0,
+					match_time: '16:00',
+					stadium: 'Ahmed bin Ali'
+				},
+				{
+					id: 6,
+					team_A: 'FRA',
+					score_A: 0,
+					team_B: 'GER',
+					score_B: 0,
+					match_time: '16:00',
+					stadium: 'Ahmed bin Ali'
+				}
+			]
+		},
+		B: {
+			name: 'Group B',
+			teams: [
+				{
+					team_name: 'England',
+					team_short: 'ENG',
+					points: 0,
+					matches_played: 0,
+					wins: 0,
+					draws: 0,
+					losses: 0,
+					goals_forward: 0,
+					goals_against: 0,
+					goal_difference: 0
+				},
+				{
+					team_name: 'Spain',
+					team_short: 'SPA',
+					points: 0,
+					matches_played: 0,
+					wins: 0,
+					draws: 0,
+					losses: 0,
+					goals_forward: 0,
+					goals_against: 0,
+					goal_difference: 0
+				},
+				{
+					team_name: 'South Korea',
+					team_short: 'SKO',
+					points: 0,
+					matches_played: 0,
+					wins: 0,
+					draws: 0,
+					losses: 0,
+					goals_forward: 0,
+					goals_against: 0,
+					goal_difference: 0
+				},
+				{
+					team_name: 'Denmark',
+					team_short: 'DEN',
+					points: 0,
+					matches_played: 0,
+					wins: 0,
+					draws: 0,
+					losses: 0,
+					goals_forward: 0,
+					goals_against: 0,
+					goal_difference: 0
+				}
+			],
+			matches: [
+				{
+					id: 1,
+					team_A: 'ENG',
+					score_A: 0,
+					team_B: 'SKO',
+					score_B: 0,
+					match_time: '13:00',
+					stadium: 'Education City'
+				},
+				{
+					id: 2,
+					team_A: 'DEN',
+					score_A: 0,
+					team_B: 'SPA',
+					score_B: 0,
+					match_time: '16:00',
+					stadium: 'Ahmed bin Ali'
+				},
+				{
+					id: 3,
+					team_A: 'SKO',
+					score_A: 0,
+					team_B: 'DEN',
+					score_B: 0,
+					match_time: '16:00',
+					stadium: 'Ahmed bin Ali'
+				},
+				{
+					id: 4,
+					team_A: 'SPA',
+					score_A: 0,
+					team_B: 'ENG',
+					score_B: 0,
+					match_time: '16:00',
+					stadium: 'Ahmed bin Ali'
+				},
+				{
+					id: 5,
+					team_A: 'SKO',
+					score_A: 0,
+					team_B: 'SPA',
+					score_B: 0,
+					match_time: '16:00',
+					stadium: 'Ahmed bin Ali'
+				},
+				{
+					id: 6,
+					team_A: 'DEN',
+					score_A: 0,
+					team_B: 'ENG',
+					score_B: 0,
+					match_time: '16:00',
+					stadium: 'Ahmed bin Ali'
+				}
+			]
+		}
+	}
 }
 
 const mainReducer = (state = INITIAL_STATE, action) => {
